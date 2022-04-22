4ae9b5023b7d553ddaca909b5f08d85e55be57fc
zuiyu <wyx1818@vip.qq.com>
2022-04-22 08:20:40 +0800

    feat: automatically add creation time to mdx files

--- script/updateFrontmatter.js
+++ script/updateFrontmatter.js
@@ -1,23 +1,32 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const fs = require('fs').promises;
 const matter = require('gray-matter');
 
 /**
+ * Inspired by Adam
  * @see https://www.adamcollier.co.uk/blog/adding-an-updated-date-to-markdown-and-mdx-posts/
  */
 const updateFrontmatter = async () => {
   const [, , ...mdFilePaths] = process.argv;
 
   mdFilePaths.forEach(async path => {
+    const now = new Date().toISOString();
+
     const file = matter.read(path);
     const { data: currentFrontmatter } = file;
 
+    // update last modified date
     if (currentFrontmatter.published === true) {
       const updatedFrontmatter = {
         ...currentFrontmatter,
-        updatedOn: new Date().toISOString(),
       };
+
+      if (!currentFrontmatter.created) {
+        updatedFrontmatter.created = now;
+      } else {
+        updatedFrontmatter.modified = now;
+      }
       file.data = updatedFrontmatter;
+
       const updatedFileContent = matter.stringify(file);
       fs.writeFile(path, updatedFileContent);
     }
